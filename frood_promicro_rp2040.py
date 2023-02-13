137ee2d2fe84084d51c47424f71a74222845872d
Shylock357 <115421933+Shylock357@users.noreply.github.com>
2023-02-13 18:17:22 +0100

    Create frood_promicro_rp2040.py

---
+++ frood_promicro_rp2040.py
@@ -0,0 +1,28 @@
+import board
+
+pinout = [
+    board.TX,
+    board.RX,
+    None,  # GND
+    None,  # GND
+    board.D2,
+    board.D3,
+    board.D4, #6
+    board.D5, #7
+    board.D6, #8
+    board.D7, #9
+    board.D8,
+    board.D9,
+    board.D21,
+    board.D23,
+    board.D20, #14
+    board.D22, #15
+    board.D26, #16
+    board.D27, #17
+    board.D28, #18
+    board.D29, #19
+    None,  # 3.3v
+    None,  # RST
+    None,  # GND
+    None,  # RAW
+]
